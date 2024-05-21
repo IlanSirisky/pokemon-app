baaafdafbda92fa301d45ee634d96c50e121ada0
IlanSirisky <ilans@moveohls.com>
2024-05-21 15:09:12 +0300

    Test filter component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import TestButtons from "./components/testComponents/TestButtons";
 import TestTypography from "./components/testComponents/TestTypography";
+import Filter from "./components/Filter/Filter";
 
 function App() {
   return (
     <>
+    <Filter label="Sort By" />
+    <Filter label="Sort By"  disabled/>
       <TestButtons />
       <TestTypography />
     </>
