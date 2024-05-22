1e9f93886cfaee15895891c9b63bf06a40a27924
IlanSirisky <ilans@moveohls.com>
2024-05-22 12:17:14 +0300

    Fix

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,21 +1,35 @@
-import React from 'react';
-import { buttonType, buttonSize } from './types';
-import { StyledButton } from './styles';
+import React from "react";
+import { buttonType, buttonSize} from "./types";
+import { StyledButton } from "./styles";
+import {CSSProperties} from "styled-components";
 
 interface ButtonProps {
-    onClick?: () => void;
-    children: React.ReactNode;
-    type: buttonType;
-    size: buttonSize;
-    disabled?: boolean;
+  onClick?: () => void;
+  children: React.ReactNode | string;
+  type: buttonType;
+  size: buttonSize;
+  disabled?: boolean;
+  style?: CSSProperties;
 }
 
-const Button: React.FC<ButtonProps> = ({ onClick, children, type, size, disabled = false }) => {
-    return (
-        <StyledButton onClick={onClick} $type={type} $size={size} disabled={disabled}>
-            {children}
-        </StyledButton>
-    );
+const Button = ({
+  onClick,
+  type = "secondary",
+  size = "medium",
+  children,
+  disabled = false,
+  style,
+}: ButtonProps) => {
+  return (
+    <StyledButton
+      style={style}
+      onClick={onClick}
+      $type={type}
+      $size={size}
+      disabled={disabled}>
+      {children}
+    </StyledButton>
+  );
 };
 
 export default Button;
