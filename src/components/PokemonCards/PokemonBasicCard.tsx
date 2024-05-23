657882d7db7ef3e55b6a68ff4c949cf702debfb5
IlanSirisky <ilans@moveohls.com>
2024-05-23 13:46:45 +0300

    Card files

---
+++ src/components/PokemonCards/PokemonBasicCard.tsx
@@ -0,0 +1,35 @@
+import { StyledBasicCard } from "./styles";
+import { CSSProperties } from "styled-components";
+import CardBody from "./CardBody";
+
+interface BasicCardProps {
+  cardTitle: string;
+  image: string;
+  subheadText?: string;
+  cornerText?: string;
+  topCornerIcon?: string;
+  style?: CSSProperties;
+}
+
+const PokemonBasicCard = ({
+  cardTitle,
+  image,
+  subheadText,
+  cornerText,
+  topCornerIcon,
+  style,
+}: BasicCardProps) => {
+  return (
+    <StyledBasicCard style={style}>
+      <CardBody
+        cardTitle={cardTitle}
+        image={image}
+        subheadText={subheadText}
+        cornerText={cornerText}
+        topCornerIcon={topCornerIcon}
+      />
+    </StyledBasicCard>
+  );
+};
+
+export default PokemonBasicCard;
