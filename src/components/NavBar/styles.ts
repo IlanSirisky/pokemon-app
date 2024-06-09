b25d0172a5022b4418c93193cbfdcf43edc7a718
IlanSirisky <ilans@moveohls.com>
2024-06-09 15:39:33 +0300

    Sticky navbar

--- src/components/NavBar/styles.ts
+++ src/components/NavBar/styles.ts
@@ -5,11 +5,17 @@ import { COLORS } from "../../styles/colors";
 
 const { Neutrals, Primary } = COLORS;
 
-export const StyledNavBar = styled(flexRow)`
+export const StyledNavBar = styled(flexRow)<{ $isVisible: boolean }>`
   justify-content: space-between;
   align-items: center;
   padding: ${cssSpacings.s12} 40px;
   background-color: ${Neutrals.White};
+  position: sticky;
+  top: 0;
+  z-index: 10; /* Adjust as needed to ensure it appears above other content */
+  box-shadow: 0 1px 1px ${COLORS.Neutrals.N100}; /* Optional: adds a subtle shadow */
+  transform: ${({ $isVisible }) => ($isVisible ? 'translateY(0)' : 'translateY(-100%)')};
+  transition: transform 0.3s ease-in-out;
 `;
 
 export const StyledMenu = styled(flexRow)`
@@ -22,6 +28,5 @@ export const ButtonsContainer = styled(flexRow)`
 
 export const ActiveButtonStyle = {
   backgroundColor: `${Primary.P50}`,
-  fontWeight: `${cssFontWeights.bold}`
-}
-
+  fontWeight: `${cssFontWeights.bold}`,
+};
